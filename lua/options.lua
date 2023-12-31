ab1be1b3712bbfdc375da485787117d33472db3a
Anand S <anandsatheeshk@gmail.com>
2023-12-31 16:01:26 +0530

    keymap type fix

--- lua/options.lua
+++ lua/options.lua
@@ -8,4 +8,4 @@ vim.opt.sidescrolloff = 8
 
 vim.g.mapleader = " "
 vim.keymap.set('i', 'jk', '<ESC>')
-vim.keymap.set('n', '<leader>pv', ':Explorer<CR')
+vim.keymap.set('n', '<leader>pv', ':Explore<CR>')
